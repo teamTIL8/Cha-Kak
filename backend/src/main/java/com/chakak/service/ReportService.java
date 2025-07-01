fb60a9c7a56dc35d6977add0f35fd24044f6c464
ishwang <his8457@naver.com>
2025-07-01 16:29:51 +0900

    제보 신청하기 구현

---
+++ backend/src/main/java/com/chakak/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.chakak.service;
+
+import java.util.List;
+
+import com.chakak.domain.Report;
+
+public interface ReportService {
+	
+	public Report save(Report report);
+
+	public List<Report> findAll();
+}
---
+++ backend/src/main/java/com/chakak/service/ReportServiceImpl.java
@@ -0,0 +1,28 @@
+package com.chakak.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.chakak.domain.Report;
+import com.chakak.repository.ReportRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReportServiceImpl implements ReportService{
+	
+	private final ReportRepository repository;
+	
+	@Override
+	public Report save(Report report) {
+		return repository.save(report);
+	}
+
+	@Override
+	public List<Report> findAll() {
+		return repository.findAll();
+	}
+
+}
